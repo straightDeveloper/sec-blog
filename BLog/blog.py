3c94eda99386193b0e5bc58cd1cf6dec7c34ede9
mostafa <mostafa@mostafa-VirtualBox.(none)>
2014-10-30 12:50:14 +0200

    before testing for what is key property

--- BLog/blog.py
+++ BLog/blog.py
@@ -5,24 +5,40 @@ from model import Page,Post
 from handler import Handler
 
 blog_url = r"/blog/(\d+)";
-
+mainPageName = "MainPage"
 
 
 class blog(Handler):
 
 	
 	mainTemplates = ["Blog/BlogPosts.html","Blog/BlogLinks.html"];
-	template = "Blog/BlogPosts.html";
-	
-	def getLinkContent(self,ID):
-		pass
+	template = mainTemplates[1];
+	ID = None;
 
-	def getContent(self,ID):
+	def getParent(self):
+		parent = None;
+		if self.ID:
+			parent = Page.get_by_id(self.ID);
+			
+		return parent;
+
+	def getLinkContent(self):
+		return Page.query(Page.parent == self.getParent()).order(-Page.timeOfCreation)
+
+	def getPostContent(self):
 		return Post.query().order(-Post.time);
 	
-	def writePage(self,ids):
-		ID = int(ids);
-		content = self.getContent(ID);
+
+	def getID(self,ids):
+		try:
+			self.ID = int(ids);
+		except:
+			if ids == mainPageName:
+				self.ID = None;
+
+
+	def writePage(self):
+		content = self.getLinkContent();
 		self.render(self.template,content=content)
 	
 	
@@ -30,10 +46,12 @@ class blog(Handler):
 	
 
 	def get(self,ids):
-		self.writePage(ids);
+		self.getID(ids);
+		self.writePage();
 
-	def post(self):
-		post = self.get_posted_data();
+	def post(self,ids):
+		self.getID(ids);
+		post = self.get_posted_link_data();
 		post.put();
 		self.redirect(self.request.url);
 	
@@ -42,8 +60,14 @@ class blog(Handler):
 
 
 
-	def get_posted_data(self):
+	def get_posted_post_data(self):
 		subject = self.get_filtered_form("subject");
 		content = self.get_filtered_form("content");
 		return Post(title=subject,text=content,parent=None);	
+
+
+	def get_posted_link_data(self):
+		name = self.get_form("name");
+		return Page(name=name,parent=self.getParent(),isPostPage=False);
+
 
