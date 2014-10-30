2cf1301ff9d677e6a3fa6b14b7395034a57f564f
mostafa <mostafa@mostafa-VirtualBox.(none)>
2014-10-30 14:13:31 +0200

    adding posts and links together

--- BLog/blog.py
+++ BLog/blog.py
@@ -4,8 +4,8 @@ import jinja2
 from model import Page,Post
 from handler import Handler
 
-blog_url = r"/blog/(\d+)";
 mainPageName = "MainPage"
+blog_url = r"/blog/(\d+|%s)"%mainPageName;
 
 
 class blog(Handler):
@@ -14,27 +14,45 @@ class blog(Handler):
 	mainTemplates = ["Blog/BlogPosts.html","Blog/BlogLinks.html"];
 	template = mainTemplates[1];
 	ID = None;
+	isLinkPage = True;
 
 	def getParent(self):
 		parent = None;
 		if self.ID:
 			parent = Page.get_by_id(self.ID);
-			
+
 		return parent;
-
-	def getLinkContent(self):
-		return Page.query(Page.parent == self.getParent()).order(-Page.timeOfCreation)
-
-	def getPostContent(self):
-		return Post.query().order(-Post.time);
 	
 
-	def getID(self,ids):
+
+
+	def getParentKey(self):
+		parent = self.getParent();
+		if parent:
+			return parent.key;
+
+
+
+	def getLinkContent(self):
+		return Page.query( Page.parent == self.getParentKey() ).order(-Page.timeOfCreation)
+
+	def getPostContent(self):
+		return Post.query( Post.parent == self.getParentKey() ).order(-Post.time);
+	
+
+	def set_ID_and_Type(self,ids):
 		try:
 			self.ID = int(ids);
+			parent = self.getParent();
+			if parent:#checking if this id exist
+				self.isLinkPage = not parent.isPostPage; 
+				return
 		except:
 			if ids == mainPageName:
 				self.ID = None;
+				return;
+		
+		self.redirect( blog_url.replace( "(\d+|%s)" % mainPageName , mainPageName ) );
 
 
 	def writePage(self):
@@ -46,11 +64,12 @@ class blog(Handler):
 	
 
 	def get(self,ids):
-		self.getID(ids);
+		self.set_ID_and_Type(ids);
+
 		self.writePage();
 
 	def post(self,ids):
-		self.getID(ids);
+		self.set_ID_and_Type(ids);
 		post = self.get_posted_link_data();
 		post.put();
 		self.redirect(self.request.url);
@@ -63,11 +82,12 @@ class blog(Handler):
 	def get_posted_post_data(self):
 		subject = self.get_filtered_form("subject");
 		content = self.get_filtered_form("content");
-		return Post(title=subject,text=content,parent=None);	
+		return Post(title=subject,text=content,parent=self.getParentKey());	
 
 
 	def get_posted_link_data(self):
 		name = self.get_form("name");
-		return Page(name=name,parent=self.getParent(),isPostPage=False);
+		isPostPage = not self.get_form("isLinkPage");
+		return Page(name=name,parent=self.getParentKey(),isPostPage=isPostPage);
 
 
